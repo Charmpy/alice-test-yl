1275d5ec54d912a61784d5c7e97b4d3642845db7
Charmpy <alex-cvc@mail.ru>
2021-04-27 00:35:19 +0500

    HW 3

--- flask_app.py
+++ flask_app.py
@@ -58,7 +58,7 @@ def handle_dialog(res, req):
 
     # если пользователь новый, то просим его представиться.
     if req['session']['new']:
-        res['response']['text'] = '(12) Привет! Назови свое имя!'
+        res['response']['text'] = 'Привет! Назови свое имя!'
         # создаем словарь в который в будущем положим имя пользователя
         sessionStorage[user_id] = {
             'first_name': None

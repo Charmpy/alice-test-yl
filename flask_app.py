a3e293d1b2b7595c91087c5ab604b63b68227837
Charmpy <alex-cvc@mail.ru>
2021-04-26 21:13:32 +0500

    second try

--- flask_app.py
+++ flask_app.py
@@ -59,6 +59,7 @@ def get_cities(req):
                 cities.append(entity['value']['city'])
     return cities
 
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
